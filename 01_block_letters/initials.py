93c56bc481e5afafa13dc2297a3e5ca2ad788b9a
Iker González <84924263+iker-gonzalez@users.noreply.github.com>
2023-03-24 12:52:44 +0100

    Create initials.py

---
+++ 01_block_letters/initials.py
@@ -0,0 +1,15 @@
+#name: Iker
+#last name: Gonzalez
+#fun fact: everyone call me Tiki
+
+full_name = """
+IIIII    GGG 
+  I     G   G
+  I     G    
+  I     GGGGG
+  I     G   G
+  I     G   G
+IIIII    GGG 
+"""
+
+print(full_name)
